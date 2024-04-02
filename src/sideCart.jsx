457a760dbab55be2963000a6babc6ed2c58e664d
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-04-02 04:33:35 +0200

    Add Bootstrap and React Bootstrap dependencies

---
+++ src/sideCart.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Button from 'react-bootstrap/Button';
+
+export default function SideCart() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Launch
+            </Button>
+
+            <Offcanvas show={show} onHide={handleClose} placement='end'>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    Some text as placeholder. In real life you can have the elements you
+                    have chosen. Like, text, images, lists, etc.
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
+    )
+}
---
+++ src/sideCartBtn.jsx
@@ -0,0 +1,6 @@
+export default function SideCartBtn() {
+
+    return (
+        <></>
+    )
+}
