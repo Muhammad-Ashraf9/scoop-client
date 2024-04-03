e11235ab244b4f76870a0c2be33e64c9344f8e08
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-04-03 20:30:50 +0200

    Refactor routes and remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import { BrowserRouter as Router , Navigate, Route, Routes } from "react-router-dom";
 
 import "./App.css";
-
-import Login from "./Pages/Login";
-import PageNotFound from "./Pages/PageNotFound";
-import { HomePage } from "./Pages/Home/";
-import { ProductList } from "./Pages/Product";
 import { AllRoutes } from "./routes/AllRoutes";
-import { Navbar } from "./components/Layout/Navbar";
 import { Footer } from "./components/Layout/Footer";
 
 function App() {
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,23 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router , Navigate, Route, Routes } from "react-router-dom";
-import { HomePage } from "../Pages/Home";
-import { ProductList } from "../Pages/Product";
-import  Login  from "../Pages/Login.jsx";
-import  PageNotFound  from "../Pages/PageNotFound";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { HomePage } from "../pages/Home";
+import { ProductList } from "../pages/Product";
+import Login from "../pages/Login.jsx";
+import PageNotFound from "../pages/PageNotFound";
 
-export const AllRoutes =  ()=>{
-
-    return(
-        <>
-             <Router>
+export const AllRoutes = () => {
+  return (
+    <>
+      <Router>
         <Routes>
           <Route index element={<HomePage />} />
           <Route path="login" element={<Login />} />
           <Route path="products" element={<ProductList />} />
           <Route path="*" element={<PageNotFound />} />
-          <Route path="Admin" element={<Navigate to="/"/> } />
+          <Route path="Admin" element={<Navigate to="/" />} />
         </Routes>
       </Router>
-        </>
-    )
-}
+    </>
+  );
+};
